531f762543673e9eca607d385a27b75a94c151c6
Piyush Anand <piyushreb@gmail.com>
2022-03-02 19:57:06 +0530

    done image section

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,8 +11,15 @@ export const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults('/search/q=JavaScript mastery&num=40')
-  }, []);
+    if(searchTerm){
+       if(location.pathname==='/videos'){
+         getResults(`/search/q=${searchTerm} videos`);
+       }
+       else{
+         getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+       }
+    }
+  }, [searchTerm, location.pathname]);
 
 
   if (isLoading) return <Loading />;
@@ -23,9 +30,9 @@ export const Results = () => {
 
     case '/search':
       return (
-        <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+        <div className="flex flex-wrap justify-between space-y-6 lg:w-8/12 sm:px-56">
           {results?.results?.map(({ link, title , description}, index) => (
-            <div key={index} className="md:2/5 w-full">
+            <div key={index} className="md:2/5 w-full ">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
                   {link.length > 30 ? link.substring(0, 30) : link}
@@ -33,7 +40,7 @@ export const Results = () => {
                 <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
                   {title}
                 </p>
-                <p className="text-lg  dark:text-gray-300 text-gray-600">
+                <p className="text-md  dark:text-gray-300 text-gray-600">
                   {description}
                 </p>
               </a>
@@ -42,7 +49,20 @@ export const Results = () => {
         </div>
       );
     case '/images':
-      return 'SEARCH';
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.image_results?.map(({image, link:{href,title}},index)=>(
+            <a href={href} className="sm:p-2 p-5" key={index} target="_blank" rel="noreferrer">
+              <div className="w-40 h-auto hover:shadow-xl">
+              <img src={image?.src} alt={title} loading="lazy" className="object-contain h-40 w-40 rounded" />
+              </div>
+              <p className="w-36 break-words text-sm mt-1 hover:underline">
+                {title}
+              </p>
+            </a>
+          ))}
+        </div>
+      );
     case '/news':
       return 'SEARCH';
     case '/videos':
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,20 +1,20 @@
-import React , { createContext , useContext , useState} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 
-export const ResultContextProvider = ({ children })=> {
-    const [results,setResults] =useState([]);
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('got');
 
     // /videos,/search, /images
-    const getResults = async (type)=>{
+    const getResults = async (type) => {
         setIsLoading(true);
 
-        const response = await fetch(`${baseUrl}${type}`,{
-            method:'GET',
-            headers:{
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
                 'x-user-agent': 'desktop',
                 'x-proxy-location': 'EU',
                 'x-rapidapi-host': 'google-search3.p.rapidapi.com',
@@ -22,17 +22,17 @@ export const ResultContextProvider = ({ children })=> {
             }
         });
         const data = await response.json();
-        
-    console.log(data);
+
+        console.log(data);
 
         setResults(data);
         setIsLoading(false);
     }
     return (
-        <ResultContext.Provider value={{ getResults, results , searchTerm, setSearchTerm, isLoading }}>
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
             {children}
         </ResultContext.Provider>
-        
+
     )
 }
 
